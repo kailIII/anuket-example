629a2ae49c9bac2e16affe93c05f165f0eb8ce23
Bertrand Lecervoisier <miniwark@gmail.com>
2012-08-16 09:41:54 +0200

    Add some tests for the example views

--- anuketexample/tests.py
+++ anuketexample/tests.py
@@ -1,17 +1,71 @@
+import os
+import sys
 import unittest
 
+from paste.deploy.loadwsgi import appconfig
 from pyramid import testing
 
 
-class ViewTests(unittest.TestCase):
+# minimum settings for tests
+here = os.path.abspath(os.path.dirname(__file__))
+settings = {
+    'pyramid.available_languages': 'en fr',
+    'mako.directories':
+        ('{0}/../anuketexample/templates'.format(here),
+        'anuket:templates'),
+    'sqlalchemy.url': 'sqlite:///anuket-example.test.db',
+    'anuket.brand_name': 'Example',
+}
+
+
+class ViewIntegrationTests(unittest.TestCase):
+    """ Integration tests for the views."""
     def setUp(self):
         self.config = testing.setUp()
+        # register the routes
+        self.config.include('anuketexample.views')
 
     def tearDown(self):
         testing.tearDown()
 
-#    def test_my_view(self):
-#        from .views import my_view
-#        request = testing.DummyRequest()
-#        info = my_view(request)
-#        self.assertEqual(info['project'], 'anuket-example')
+    def test_routes(self):
+        """ Test the routes of the views."""
+        request = testing.DummyRequest()
+        self.assertEqual(request.route_path('hello'), '/hello')
+        self.assertEqual(request.route_path('hello_admin'), '/hello/admin')
+
+    def test_hello_world_view(self):
+        """ Test the response of the `root_view`."""
+        from anuketexample.views import hello_world
+        request = testing.DummyRequest()
+        response = hello_world(request)
+        self.assertEqual(response, {'hello': u'Hello World!'})
+
+    def test_hello_admin_view(self):
+        """ Test the response of the `root_view`."""
+        from anuketexample.views import hello_admin
+        request = testing.DummyRequest()
+        response = hello_admin(request)
+        self.assertEqual(response, {'hello': u'Hello Admin!'})
+
+
+class ViewFunctionalTests(unittest.TestCase):
+    """ Functional tests for the views."""
+    def setUp(self):
+        from anuketexample import main
+        app = main({}, **settings)
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+
+    def test_hello_world_page(self):
+        """ Test the hello world page response for everybody."""
+        response = self.testapp.get('/hello', status=200)
+        self.assertTrue('Hello World!' in response.body)
+
+    def test_hello_admin_page(self):
+        """ Test than the hello admin page is forbiden for non logged users."""
+        response = self.testapp.get('/hello/admin', status=302)
+        redirect = response.follow()
+        self.assertEqual(redirect.status, '200 OK')
+        self.assertEqual(redirect.request.path, '/login')
+        self.assertTrue('You are not connected.' in redirect.body)
--- setup.py
+++ setup.py
@@ -11,29 +11,33 @@ requires = [
     'anuket',
     ]
 
+tests_require = [
+    'WebTest',
+    ]
+
 setup(name='anuket-example',
-      version='0.1',
-      description='Anuket example application',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
+    version='0.2b',
+    description='Anuket example application',
+    long_description=README + '\n\n' +  CHANGES,
+    classifiers=[
         "Programming Language :: Python",
         "Framework :: Pylons",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         ],
-      author='LDPL - Laboratoire Départemental de Préhistoire du Lazaret',
-      author_email='opensource@lazaret.unice.fr',
-      url='http://github.com/lazaret/anuket-example',
-      license='LICENSE.txt',
-      keywords='web wsgi pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
-      test_suite="anuketexample",
-      entry_points = """\
-      [paste.app_factory]
-      main = anuketexample:main
-      """,
-      )
+    author='LDPL - Laboratoire Départemental de Préhistoire du Lazaret',
+    author_email='opensource@lazaret.unice.fr',
+    url='http://github.com/lazaret/anuket-example',
+    license='LICENSE.txt',
+    keywords='web wsgi pyramid',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=tests_require,
+    test_suite="anuketexample",
+    entry_points = """\
+    [paste.app_factory]
+    main = anuketexample:main
+    """,
+    )
